89200c354a1141f863da3a8373d07903926ef659
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2022-11-14 23:26:49 +0700

    update

---
+++ createData.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+
+const createData = async () => {
+  let data = JSON.parse(fs.readFileSync("db.json"));
+
+  let newData = await csv().fromFile("pokemon.csv");
+  newData = Array.from(newData);
+  newData = newData.slice(0, 721);
+
+  newData = newData.map((pokemon, index) => {
+    const pokemonName =
+      pokemon.name.charAt(0).toLowerCase() + pokemon.name.slice(1);
+    console.log(newData);
+    if (data.type2) {
+      return {
+        id: `${index + 1}`,
+        name: pokemonName,
+        height: `${pokemon.height_m} '`,
+        weight: `${pokemon.weight_kg} .lbs`,
+        abilities: pokemon.abilities,
+        types: [pokemon.type1, pokemon.type2],
+        url: `http://localhost:5000/images/${index + 1}.jpg`,
+      };
+    } else {
+      return {
+        id: `${index + 1}`,
+        name: pokemonName,
+        height: `${pokemon.height_m} '`,
+        weight: `${pokemon.weight_kg} .lbs`,
+        abilities: pokemon.abilities,
+        types: [pokemon.type1],
+        url: `http://localhost:5000/images/${index + 1}.jpg`,
+      };
+    }
+  });
+
+  data.totalPokemons = newData.length;
+
+  data.data = newData;
+
+  fs.writeFileSync("db.json", JSON.stringify(data));
+};
+
+createData();
